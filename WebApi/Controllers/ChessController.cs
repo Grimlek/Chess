7daadfd207e4138de3d6e464c905191622e7a5ca
grimlek <charlie_sexton@yahoo.com>
2020-02-16 21:17:41 -0500

    Stage 3: Initial Check-In for WebAPI project

---
+++ WebApi/Controllers/ChessController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Chess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ChessController : ControllerBase
+    {
+
+        /// <summary>
+        ///     Get the available moves for a specific chess piece at a starting position.
+        /// </summary>
+        /// <remarks>
+        ///     Sample Request:
+        ///
+        ///         GET /GetAvailableMoves/rook?startingPosition=d3
+        ///         
+        ///     Chess Pieces: rook, queen, king, and bishop<br/>
+        ///     Starting Positions are in chess notation with the standard 8x8 chess board, ex. a3, b2, c4, h8
+        /// </remarks>
+        /// <param name="chessPieceName" in="path" required="true" type="string"></param> 
+        /// <param name="startingPosition" in="query" required="true" type="string"></param> 
+        /// <returns>Returns the available chess moves in CSV format.</returns>
+        /// <response code="200">Successful operation</response>  
+        /// <response code="400">If the starting position is invalid.</response>  
+        /// <response code="404">If the chess piece is not found.</response>  
+        [HttpGet("GetAvailableMoves/{chessPieceName}")]
+        public IActionResult GetAvailableMoves(string chessPieceName, [Required] string startingPosition)
+        {
+            PieceBase chessPiece = PieceFactory.CreateChessPiece(chessPieceName);
+            if (chessPiece == null)
+            {
+                return NotFound("Chess piece '" + chessPieceName + "' not found.");
+            }
+
+            Board chessBoard = new Board();
+
+            Square startingSquare = chessBoard.GetSquare(startingPosition);
+            if (startingSquare == null)
+            {
+                return BadRequest("Starting position was not found on the chess board.");
+            }
+
+            startingSquare.piece = chessPiece;
+
+            List<Square> availableMoves = chessBoard.GetAvailableMoves(startingSquare);
+
+            string availableMovesOutput = Board.ConvertToCSV(availableMoves);
+            string json = "{ 'availableChessMoves': '" + availableMovesOutput + "' }";
+
+            return Ok(json);
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Chess API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Charles Sexton",
+                        Email = "charlie_sexton@yahoo.com",
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chess API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
